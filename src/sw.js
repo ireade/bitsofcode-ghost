d50874234d194b1e3c3d047a22f80c0d2746521c
Ire Aderinokun <iaderinokun@gmail.com>
2018-12-10 11:03:40 +0000

    Add Paul Irish fix to sw

--- src/sw.js
+++ src/sw.js
@@ -41,29 +41,22 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request)
+      .then((response) => {
 
-  // Ignore resources from external sources
-  if (!e.request.url.includes(e.request.referrer)) {
-    e.respondWith(fetch(e.request));
-  } else {
-    e.respondWith(
-      caches.match(e.request)
-        .then((response) => {
+        if (response) {
+          console.log("[ServiceWorker] Found in cache", e.request.url);
+          return response;
+        }
 
-          if (response) {
-            console.log("[ServiceWorker] Found in cache", e.request.url);
-            return response;
-          }
+        return fetch(e.request)
+          .then((fetchResponse) => fetchResponse)
+          .catch((err) => {
+            const isHTMLPage = e.request.method === "GET" && e.request.headers.get("accept").includes("text/html");
+            if (isHTMLPage) return caches.match("/offline/");
+          })
 
-          return fetch(e.request)
-            .then((fetchResponse) => fetchResponse)
-            .catch((err) => {
-              const isHTMLPage = e.request.method === "GET" && e.request.headers.get("accept").includes("text/html");
-              if (isHTMLPage) return caches.match("/offline/");
-            })
-
-        }) // end caches.match(e.request)
-    ); // end e.respondWith
-  }
-  
+      }) // end caches.match(e.request)
+  ); // end e.respondWith
 });
